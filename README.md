9697c517a945aff25ba23fc6121f9f5fa951d541
Alexander Færøy <ahf@0x90.dk>
2016-05-09 22:26:20 +0200

    Use edoc for documentation.

---
+++ README.md
@@ -0,0 +1,48 @@
+
+
+# Luke #
+
+__Version:__ 1.2.0
+
+__Authors:__ Alexander Færøy ([`ahf@0x90.dk`](mailto:ahf@0x90.dk)).
+
+Luke is an Erlang NIF for the post-quantum key exchange: A New Hope.
+
+For more information about A New Hope, including the paper itself, see [github.com/tpoeppelmann/newhope](https://github.com/tpoeppelmann/newhope).
+
+
+### <a name="Example_Usage">Example Usage</a> ###
+
+1. Alice generates a new keypair and sends her public key to Bob.
+
+```erlang
+#{ secret := AliceSecretKey, public := AlicePublicKey } = luke:keypair().
+```
+
+2. Bob uses Alice's public key to compute the shared secret and a public key that he then sends back to Alice.
+
+```erlang
+#{ shared := BobSharedSecret, public := BobPublicKey } = luke:sharedb(AlicePublicKey).
+```
+
+3. Alice computes the shared secret using her own secret key and Bob's public
+key.
+
+```erlang
+AliceSharedSecret = luke:shareda(AliceSecretKey, BobPublicKey).
+```
+
+4. You can now verify that the shared secret is the same.
+
+```erlang
+AliceSharedSecret =:= BobSharedSecret.
+```
+
+
+
+## Modules ##
+
+
+<table width="100%" border="0" summary="list of modules">
+<tr><td><a href="https://lab.baconsvin.org/ahf/luke/blob/develop/doc/luke.md" class="module">luke</a></td></tr></table>
+
--- src/luke.erl
+++ src/luke.erl
@@ -31,16 +31,50 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+%% @doc Generate a New Hope keypair.
+%%
+%% This function generates and returns a new keypair for the New Hope key
+%% exchange scheme. The return value is a map to ensure that the public key
+%% isn't misused as the secret key and vice versa.
+%%
+%% The public key generated here is an 1824 byte binary.
+%%
+%% @end
 -spec keypair() -> keypair().
 keypair() ->
     {Public, Secret} = luke_nif:keypair(),
     #{ public => Public, secret => Secret }.
 
+%% @doc Generate shared secret and ephemeral public key from a public key.
+%%
+%% This function takes the public key of the handshake initiator and computes
+%% the shared secret and an ephemeral public key.
+%%
+%% The ephemeral public key should be send back to the handshake initiator to
+%% complete the handshake. The ephemeral public key generated here is a 2048
+%% byte binary.
+%%
+%% The shared secret is a 32 byte binary that can be used as key for a
+%% symmetric cipher.
+%%
+%% The return value of this function is a map to ensure that the return values
+%% are not used incorrectly.
+%%
+%% @end
 -spec sharedb(PublicA :: public_key()) -> #{ shared => shared(), public => public_key() }.
 sharedb(PublicA) ->
     {Shared, PublicB} = luke_nif:sharedb(PublicA),
     #{ shared => Shared, public => PublicB }.
 
+%% @doc Compute the shared secret from a secret key and a public key.
+%%
+%% This function takes the secret key of the initiator and the ephemeral public
+%% key of the responder and computes the shared secret.
+%%
+%% The shared secret is a 32 byte binary that can be used as key for a
+%% symmetric cipher.
+%%
+%% @end
 -spec shareda(SecretA :: secret_key(), PublicB :: public_key()) -> shared().
 shareda(SecretA, PublicB) ->
     luke_nif:shareda(SecretA, PublicB).
